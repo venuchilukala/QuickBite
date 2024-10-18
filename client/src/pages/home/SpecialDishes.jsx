34d809978f6086aeed10b54cc48fad9384984553
venuchilukala <venuchilukala07@gmail.com>
2024-10-18 19:01:48 +0530

    Added Category Section

---
+++ client/src/pages/home/SpecialDishes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SpecialDishes = () => {
+  return (
+    <div className='section-container my-20'>
+      <div className="text-left">
+        <p className="subtitle">Special Dishes</p>
+        <h2 className="title md:w-[520px]">Standout Dishes From Our Menu</h2>
+      </div>
+    </div>
+  )
+}
+
+export default SpecialDishes
